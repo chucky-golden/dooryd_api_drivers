d822311bd870e4afbcdf226ade1ae4a85967a80b
chucky-golden <chukschibyke92@gmail.com>
2024-01-18 05:41:37 +0100

    new updates

--- app.ts
+++ app.ts
@@ -14,7 +14,6 @@ const startServer = async () => {
 
     app.use(cors())
 
-    app.use(express.json())
     app.use(express.json({ limit: '10mb' }));
     app.use(express.urlencoded({ extended: true }))
 
